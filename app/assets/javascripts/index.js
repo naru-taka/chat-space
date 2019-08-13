eabe65bc2659fdd8ac6f90cdbb730d27f3479fd0
naru-taka <aloha.46.naru@me.com>
2019-08-13 14:20:50 +0900

    HTMLをビューに追加

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,29 +1,36 @@
 $(function() {
 
-  var user_search_list = $(".chat-group-form__field--right");
   function appendUser(user) {
-    var html = `<div class="chat-group-user clearfix">
-                  <p class="chat-group-user__name">${user.name}</p>
-                    <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
-                </div>
+    var html = `
+               <div class="chat-group-form__field--right--search">
+                  <div class="chat-group-user clearfix">
+                    <p class="chat-group-user__name">${user.name}</p>
+                      <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
+                  </div>
+               </div>
                `
-    user_search_list.append(html);
+    return html;
   };
 
   $(function() {
     $("#user-search-field").on("keyup", function() {
       var input = $("#user-search-field").val();
       var href = window.location.href
-      console.log(input);
+      // console.log(input);
       $.ajax({
         type: 'GET',
         url: '/users',
         data: { keyword: input },
         dataType: 'json'
       })
-      .done(function(data) {
-        var html = appendUser(data);
-        $('chat-group-form__field--right').append(html);
+      .done(function(users) {
+        $(".user-search-result").empty();
+        if (users.length !== 0) {
+          users.forEach(function(user){
+            var html = appendUser(user);
+            $(".user-search-result").append(html);
+          });
+        }
       })
       .fail(function(){
         alert('ユーザー検索に失敗しました');
