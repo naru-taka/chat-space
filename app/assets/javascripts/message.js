3383c6eeb633983ccaa5a4dc5bb9d1b9172dd05a
naru-taka <aloha.46.naru@me.com>
2019-08-16 23:07:25 +0900

    undifined 対応記述

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -35,12 +35,20 @@ $(function(){
       contentType: false
     })
     .done(function(data){
+      
+      if (data.content === nil){
+      
+      alert('error');
+      $( ".form__submit").prop( "disabled", false );
+      }
+      else{
       var html = buildHTML(data);
       $('.messages').append(html);
       $( ".form__submit").prop( "disabled", false );
       $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
       $('form__message').val('');
       $('.hidden').val('');
+      }
     })
     .fail(function(){
       alert('error');
