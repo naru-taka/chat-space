e2b375122a21c17939bbc0f1cc841e8a3a33eb20
naru-taka <aloha.46.naru@me.com>
2019-08-16 19:22:56 +0900

    バケットを新たに作成したため

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,6 +12,6 @@ CarrierWave.configure do |config|
     region: 'ap-northeast-1'
   }
 
-  config.fog_directory  = 'naru-taka'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/naru-taka'
+  config.fog_directory  = 'upload-naru'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/upload-naru'
 end
